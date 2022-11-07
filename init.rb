37ce31f8f099d13bbf373884f8db5962b0f8ea36
Pavel Dušánek <dusanek@iquest.cz>
2022-11-07 14:21:22 +0100

    Plugin init.rb is now called from to_prepare block in Redmine::PluginLoader

--- init.rb
+++ init.rb
@@ -1,36 +1,10 @@
 require 'redmine'
 
-## Taken from lib/redmine.rb
-#if RUBY_VERSION < '1.9'
-#  require 'faster_csv'
-#else
-#  require 'csv'
-#  FCSV = CSV
-#end
-
-if Rails::VERSION::MAJOR < 3
-  require 'dispatcher'
-  object_to_prepare = Dispatcher
-else
-  object_to_prepare = Rails.configuration
-  # if redmine plugins were railties:
-  # object_to_prepare = config
-end
-
-object_to_prepare.to_prepare do
-  require_dependency 'project'
-  require_dependency 'principal'
-  require_dependency 'user'
-  Project.send(:include, RedmineTimesheetPlugin::Patches::ProjectPatch)
-  User.send(:include, RedmineTimesheetPlugin::Patches::UserPatch)
-
-  # Needed for the compatibility check
-  begin
-    require_dependency 'time_entry_activity'
-  rescue LoadError
-    # TimeEntryActivity is not available
-  end
-end
+require_dependency 'project'
+require_dependency 'principal'
+require_dependency 'user'
+Project.send(:include, RedmineTimesheetPlugin::Patches::ProjectPatch)
+User.send(:include, RedmineTimesheetPlugin::Patches::UserPatch)
 
 unless Redmine::Plugin.registered_plugins.keys.include?(:redmine_timesheet_plugin)
   Redmine::Plugin.register :redmine_timesheet_plugin do
@@ -42,7 +16,7 @@ unless Redmine::Plugin.registered_plugins.keys.include?(:redmine_timesheet_plugi
 
     version '0.7.0'
     requires_redmine :version_or_higher => '0.9.0'
-    
+
     settings(:default => {
                'list_size' => '5',
                'precision' => '2',
